1b68022c15fce0f7011198745a0167bcb1be25bd
WillV1 <wmvanhook@gmail.com>
2020-07-15 20:32:23 -0400

    Update server for deployment

--- server/server.js
+++ server/server.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 const publicPath = path.join(__dirname, '..', 'public');
-app.use(express.static(publicPath));
+
+app.use('/static', express.static(path.join(__dirname, 'client/build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'));
